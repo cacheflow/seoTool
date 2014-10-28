b0ff629b30fbeb92a18e5f95013d33751bf997ea
Lex Alexander <l.alexander10@gmail.com>
2014-10-28 16:33:42 -0700

    adding style files.

--- Gemfile
+++ Gemfile
@@ -6,13 +6,13 @@ gem 'rails', '4.1.6'
 # Use postgresql as the database for Active Record
 gem 'pg'
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
 gem "httparty"
 gem 'bootstrap-sass'
+gem "sass-rails"
 gem 'did_you_mean', group: [:development, :test]
 gem "figaro"
 gem "bootstrap_form"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,17 +9,7 @@ class UsersController < ApplicationController
     api
   end
 
-    def api 
-      @websiteurl = User.last.website
-      client = Linkscape::Client.new(:accessID => ENV["ACCESS_ID"], :secret => ENV["API_KEY"])
-      @linkscape = client.urlMetrics(@websiteurl, :cols => :all)
-      @toppages = client.topPages(@websiteurl, :page, :cols => :all, :limit => 5)
-      @anchorlinks = client.anchorMetrics(@websiteurl, :cols => :all, :scope => "page_to_domain", :filters => :external, :sort => :domains_linking_page, :limit => 5, :scope => :phrase_to_page)
-      @top = client.allLinks(@websiteurl, :urlcols => [:title, :url, :page_authority, :domain_authority], :linkcols => :all, :filters => :external, :limit => 5, :scope => :page_to_domain)
-      return 
-    end 
-
-
+ 
 
 
 
@@ -31,15 +21,15 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-   end
+  end
   
 
   def create
     
     @user = User.new(params.require(:user).permit(:website))
-     @user.save
+    @user.save
     redirect_to users_path
-   end
+  end
 
 
 
@@ -55,6 +45,17 @@ class UsersController < ApplicationController
   end
   
   
+
+private 
+     def api 
+      @websiteurl = User.last.website
+      client = Linkscape::Client.new(:accessID => ENV["ACCESS_ID"], :secret => ENV["API_KEY"])
+      @linkscape = client.urlMetrics(@websiteurl, :cols => :all)
+      @toppages = client.topPages(@websiteurl, :page, :cols => :all, :limit => 5)
+      @anchorlinks = client.anchorMetrics(@websiteurl, :cols => :all, :scope => "page_to_domain", :filters => :external, :sort => :domains_linking_page, :limit => 5, :scope => :phrase_to_page)
+      @top = client.allLinks(@websiteurl, :urlcols => [:title, :url, :page_authority, :domain_authority], :linkcols => :all, :filters => :external, :limit => 5, :scope => :page_to_domain)
+      return 
+    end 
 
 
 
