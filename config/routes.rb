b317f5a6737161ab24c6f06d750773adebe72859
Lex <l.alexander10@gmail.com>
2014-11-03 09:57:48 -0800

    namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
 
   root 'users#new'
   
+  scope do 
+  	resources :users, path: "results"
+  end 
+
+
   # scope do
   #   resources :users, path: 'results'
   # end
   
   # resources :sessions
-
-resources :users
 
 end
